7695ea2b128d7108612b0b703811b701230e4b6e
rbtech <mail.rbtech@gmail.com>
2025-12-27 01:33:49 +0700

    Update Dashboard V1

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,27 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Login Routes
+$routes->get('login', 'Auth::login');
+$routes->post('login/auth', 'Auth::attempt');
+
+// app/Config/Routes.php
+
+$routes->group('admin', ['filter' => 'authGuard'], function($routes) {
+    $routes->get('/', 'Admin\Dashboard::index');
+    
+    // CRUD Banners
+    $routes->resource('banners', ['controller' => 'Admin\Banners']);
+    
+    // CRUD Fleets
+    $routes->resource('fleets', ['controller' => 'Admin\Fleets']);
+    
+    // CRUD News
+    $routes->resource('news', ['controller' => 'Admin\News']);
+    
+    // Settings / Company Profile
+    $routes->get('settings', 'Admin\Settings::index');
+    $routes->post('settings/update', 'Admin\Settings::update');
+});
+
