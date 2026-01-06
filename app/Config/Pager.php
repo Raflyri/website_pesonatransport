16647b7d2319430f2697432228954e4d17b678c9
rbtech <mail.rbtech@gmail.com>
2026-01-06 10:21:01 +0700

    Bug fixing search feature

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,7 +24,7 @@ class Pager extends BaseConfig
         'default_full'   => 'CodeIgniter\Pager\Views\default_full',
         'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
         'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-        'round_pagination' => 'App\Views\Pagers\round_pagination',
+        'round_pagination' => 'App\Views\pagers\round_pagination',
     ];
 
     /**
